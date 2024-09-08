71bd4d388d6d6c01ff3f73a0f0d312ba73529176
Anthony Chilan <anthony.chilanp@gmail.com>
2024-09-07 20:12:57 -0500

    Update astro.config.mjs with site and base configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site:"https://achilan.github.io",
+  base: "movie-finder",
   integrations: [tailwind(), react()],
   output: 'server',
   adapter: cloudflare({
